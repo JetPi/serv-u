2d22954e31d7023894b660bf5fc12db5d39f79ab
Ibrahim Zarraga <ibrahimzarragaam@gmail.com>
2022-09-07 17:21:54 -0400

    Añadí esqueleto basico de los usuarios, todavía está en progreso

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,23 @@
 from flask_sqlalchemy import SQLAlchemy
+from enum import Enum
 
 db = SQLAlchemy()
 
+class UserNature(Enum):
+    user = "user",
+    moderator = "moderator"
+
 class User(db.Model):
+    # Identifications
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    username = db.Column(db.String(100), unique=False, nullable=False)
+    userNature = db.Column(db.Enum(UserNature), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    # Verification
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +26,7 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "username": self.username,
+            "is_active": self.is_active,
             # do not serialize the password, its a security breach
         }
