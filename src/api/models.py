2d7aa652a18a16c877c5edf3561c46f2b7b33e95
Ibrahim Zarraga <ibrahimzarragaam@gmail.com>
2022-09-20 11:34:04 -0400

    Final testing done, hopefully I wont get yelled at

--- src/api/models.py
+++ src/api/models.py
@@ -22,7 +22,7 @@ class User(db.Model):
     # Identifications
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    username = db.Column(db.String(100), unique=True, nullable=False)
+    username = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(200), nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, default=True)
     role = db.Column(db.Enum(Role), nullable=False)
