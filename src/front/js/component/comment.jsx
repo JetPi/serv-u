2e2a807d63ebdf82765c22b30e26da1f964b3a26
oriana calderon <orianasofia86@gmail.com>
2022-10-01 22:35:31 -0400

    it's done the comments in home view

--- src/front/js/component/comment.jsx
+++ src/front/js/component/comment.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import PropTypes from 'prop-types';
 import { Context } from "../store/appContext";
-import { Link, useNavigate } from "react-router-dom";
 import "../../styles/comment.css";
 
 
@@ -11,44 +10,100 @@ export const Comment = () => {
 
     useEffect(() => { { actions.getComment() } }, [])
 
+    const filtrarPorRating=(obj)=> {
+        if ('rating' in obj && (obj.rating) >= 4 ){
+          return true;
+        } else {    
+          return false;
+        }
+      }
+
+    let arr = store.comments
+
+    const arrPorRating = arr.filter(filtrarPorRating);
+
+    const goodComments = arrPorRating.slice(0, 6);
+
+    const star4 = [
+        <i className="fas fa-star star-color"></i>,
+        <i className="fas fa-star star-color"></i>,
+        <i className="fas fa-star star-color"></i>,
+        <i className="fas fa-star star-color"></i>,
+        <i className="far fa-star"></i>
+
+    ]
+
+    const star5 = [
+        <i className="fas fa-star star-color"></i>,
+        <i className="fas fa-star star-color"></i>,
+        <i className="fas fa-star star-color"></i>,
+        <i className="fas fa-star star-color"></i>,
+        <i className="fas fa-star star-color"></i>
+    ]
+
+    const stars = (rating) => {
+
+        if (rating == 4) {
+            return star4
+        }
+        else if (rating == 5) {
+            
+            return star5
+        }
+        else {
+            return false
+        }
+
+    }
+
+
+
     return (
 
-        <div className="container-fluid d-flex flex-column text-center justify-content-center align-items-center">
+        <div className="container d-flex flex-column text-center justify-content-center align-items-center">
+            <div className="my-5">
+                <h2 className="title_comment_component">
+                    Experiencias de nuestros clientes
+                </h2>
+            </div>
 
 
-            {store.comments?.map((comentario, index) => {
+            {goodComments.map((comment, index) => {
                 return (
 
 
                     <div className="row my-3 div_comentario" key={index}>
-                        <div className="col-md-4 div_foto_comentario">
-                            <img src={comentario.user_data?.profile_photo_url} className="img_comment_home" alt="foto" />
-                        </div>
-                        <div className="col-md-8 ">
-                            <div className="row card-body d-flex mx-2">
-                                <div className="row">
-                                    <h5 className="card-title">
-                                        {comentario.user_data?.username}
-                                    </h5>
-                                </div>
-                                <div className="row">
-                                    <p className="card-text">
-                                        {comentario.observation}
-                                    </p>
-                                </div>
-                                <div className="row">
-                                    <p className="card-text">
-                                        <small className="text-muted">
-                                            {comentario.rating}/5
-                                        </small>
-                                    </p>
-                                </div>
 
-
-
+                        <div className="col-12 d-flex">
+                            <div className="col-4 div_foto_comentario">
+                                <img src={comment.user_data?.profile_photo_url} className="img_comment_home" alt="foto" />
                             </div>
-
+                            <div className="col-8">
+                                <div className="row div-username-and-stars">
+                                    <div className="col-5">
+                                        <h5 className="card-user-title">
+                                            {comment.user_data?.username}
+                                            
+                                        </h5>
+                                    </div>
+                                    <div className="col-7">
+                                        <p className="card-text rating_stars">
+                                            
+                                                {stars(comment.rating)}
+                                            
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <p className="card-text-observation">
+                                            {comment.observation}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+
                     </div>
 
                 );
