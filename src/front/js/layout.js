e6598b9c76d1b5fd24de0cdc1e2335c9593a8d5b
Ibrahim Zarraga <ibrahimzarragaam@gmail.com>
2022-10-05 15:40:46 -0400

    Footer added, please watch out for any weird behaviour

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -16,37 +16,36 @@ import { SearchResult } from "./pages/results.jsx";
 
 import { Navbar } from "./component/navbar";
 import { ServiceComment } from "./component/serviceComment.jsx";
+import { Footer } from "./component/footer.jsx";
 
-
-
-
-
-
-//create your first component
 const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
     return (
-        <div>
+        <div >
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
+
                     <Navbar />
-                    <Routes>
-                        <Route element={<Home />} path="/" />
-                        <Route element={<Signup />} path="/signup" />
-                        <Route element={<Login />} path="/login" />
-                        <Route element={<Section />} path="/section" />
-                        <Route element={<Faq />} path="/faq" />
-                        <Route element={<Post_service />} path="/post_service" />
-                        <Route element={<Profile />} path="/profile" />
-                        <Route element={<ServiceInfo />} path="/service/:id" />
-                        <Route element={<ServiceComment services_id={1} />} path="/user/comments" />
-                        <Route element={<ActiveUser />} path="/user" />
-                        <Route element={<SearchResult />} path="/services/search" />
-                        <Route element={<h1>Not found!</h1>} />
-                    </Routes>
+                    <div className="testFooter">
+                        <Routes>
+                            <Route element={<Home />} path="/" />
+                            <Route element={<Signup />} path="/signup" />
+                            <Route element={<Login />} path="/login" />
+                            <Route element={<Section />} path="/section" />
+                            <Route element={<Faq />} path="/faq" />
+                            <Route element={<Post_service />} path="/post_service" />
+                            <Route element={<Profile />} path="/profile" />
+                            <Route element={<ServiceInfo />} path="/service/:id" />
+                            <Route element={<ServiceComment services_id={1} />} path="/user/comments" />
+                            <Route element={<ActiveUser />} path="/user" />
+                            <Route element={<SearchResult />} path="/services/search" />
+                            <Route element={<h1>Not found!</h1>} />
+                        </Routes>
+                        <Footer />
+                    </div>
                 </ScrollToTop>
             </BrowserRouter>
         </div>
