8e73ff43f0820ee727c35bed6fb017a8830ccb14
Victor Seidman <victorseidman@gmail.com>
2022-10-05 15:42:57 -0300

    Listo

--- src/front/js/component/ordenes.jsx
+++ src/front/js/component/ordenes.jsx
@@ -1,5 +1,8 @@
 import React, { useState, useContext } from "react";
 import { Context } from "../store/appContext";
+import { Alert } from "./alert.jsx";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 
 
@@ -19,14 +22,33 @@ export const OrderModal = ({ services_id }) => {
 
     const createOrder = () => {
         if (orderState.direccion.trim() && orderState.observacion.trim() == "") {
-            return alert("Completa los campos vacios")
+            return toast("Completa los campos vacios", {
+                position: 'top-center',
+                autoClose: true,
+                type: 'warning',
+                closeButton: true,
+                closeOnClick: true
+            })
         } else {
             const orderData = { ...orderState, services_id }
             actions.addOrders(orderData).then((response) => {
                 if (response) {
-                    return alert("Servicio contratado")
+                    //return alert("Servicio contratado")
+                    toast("Exito", {
+                        position: 'top-center',
+                        autoClose: true,
+                        type: 'success',
+                        closeButton: true,
+                        closeOnClick: true
+                    })
                 } else
-                    return alert("Hubo un error")
+                    toast("Hubo un error", {
+                        position: 'top-center',
+                        autoClose: true,
+                        type: 'error',
+                        closeButton: true,
+                        closeOnClick: true
+                    })
             }
             )
         }
@@ -34,6 +56,7 @@ export const OrderModal = ({ services_id }) => {
 
     return (
         <>
+            <ToastContainer />
             <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Contratar</button>
 
             <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -46,15 +69,15 @@ export const OrderModal = ({ services_id }) => {
                         <div className="modal-body">
                             <form>
                                 <div className="mb-3">
-                                    <label for="recipient-name" className="col-form-label">Direccion: </label>
+                                    <label htmlFor="recipient-name" className="col-form-label">Direccion: </label>
                                     <input type="text" className="form-control" onChange={handleChange} value={orderState.direccion} name="direccion" id="recipient-name" />
                                 </div>
                                 <div className="mb-3">
-                                    <label for="message-text" className="col-form-label">Observacion:</label>
+                                    <label htmlFor="message-text" className="col-form-label">Observacion:</label>
                                     <textarea className="form-control" onChange={handleChange} value={orderState.observacion} name="observacion" id="message-text"></textarea>
                                 </div>
                                 <div className="mb-3">
-                                    <label for="message-text" className="col-form-label">Metodo de pago: Efectivo <br>
+                                    <label htmlFor="message-text" className="col-form-label">Metodo de pago: Efectivo <br>
                                     </br>Pago movil:
                                         <ul>
                                             <li>Cedula: J-0673637-3</li>
--- src/front/js/pages/post_service.jsx
+++ src/front/js/pages/post_service.jsx
@@ -169,7 +169,7 @@ export const Post_service = () => {
                     <div className="modal-dialog">
                         <div className="modal-content">
                             <div className="modal-header">
-                                <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <h1 className="modal-title fs-5" id="exampleModalLabel">Sube una foto</h1>
                                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div className="modal-body">
