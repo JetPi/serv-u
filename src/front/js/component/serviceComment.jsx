02f17c665ee2266b5525bcba6b32d389d4cda059
oriana calderon <orianasofia86@gmail.com>
2022-10-06 01:59:19 -0400

    Se habian borrado algunos parentesis

--- src/front/js/component/serviceComment.jsx
+++ src/front/js/component/serviceComment.jsx
@@ -34,7 +34,7 @@ export const ServiceComment = (props) => {
                 })
             } else if (Validar()) {
                 actions.sendComment({ observation: commentData.comment, services_id: services_id, rating: commentData.rating })
-                setCommentData({comment: ""});
+                setCommentData({ comment: "" });
             }
         }
     };
@@ -160,76 +160,78 @@ export const ServiceComment = (props) => {
                                 onKeyDown={handleKey}
                             />
 
-        <div className="container-fluid d-flex flex-column text-center justify-content-center align-items-center">
-            <div className="row card py-2 shadow-lg comment-card">
-                <div className="col-12 d-flex justify-content-center flex-column">
-                    <div className="col-12 d-flex flex-row p-1">
-                        {/* Stars, its good how I made them */}
-                        {starsState.stars.map((element, index) => {
-                            return (
-                                <button
-                                    className="button-clearer"
-                                    key={index}
-                                    onMouseOver={() => changeStars(index + 1)}
-                                    onMouseOut={() => changeStars(0)}
-                                    onClick={() => changeRating(index + 1)}>
-                                    <i className={`${element} fa-star`}></i>
-                                    
-                                </button>
-                            )
-                        })}
-                    </div>
-                    </div>
+                            <div className="container-fluid d-flex flex-column text-center justify-content-center align-items-center">
+                                <div className="row card py-2 shadow-lg comment-card">
+                                    <div className="col-12 d-flex justify-content-center flex-column">
+                                        <div className="col-12 d-flex flex-row p-1">
+                                            {/* Stars, its good how I made them */}
+                                            {starsState.stars.map((element, index) => {
+                                                return (
+                                                    <button
+                                                        className="button-clearer"
+                                                        key={index}
+                                                        onMouseOver={() => changeStars(index + 1)}
+                                                        onMouseOut={() => changeStars(0)}
+                                                        onClick={() => changeRating(index + 1)}>
+                                                        <i className={`${element} fa-star`}></i>
 
-                        </div>
-                        {/* Published Comments */}
-                        <div className="container-fluid row div-service-comment">
-                            {/* Header */}
-                            <div className="border-bottom items border-0">
-                                {store.comments.length <= 0
-                                    ? "No hay comentarios en esta publicación"
-                                    : `${store.comments.length} Comentarios`}
-                            </div>
-                            {/* Comment Generator */}
-                            {store.comments.map((comentario, index) => {
-                                let commentStars = []
-                                for (let index = 0; index < starsState.stars.length; index++) {
-                                    if (index < comentario.rating) {
-                                        const element = starVariant
-                                        commentStars.push(element)
-                                    } else {
-                                        commentStars.push("fa-regular")
-                                    }
-                                }
-                                return (
-                                    <div key={index} className="border-bottom d-flex align-item-center py-2 col-lg-6 col-sm-12">
-                                        <div className="w-100 ">
-                                            <div className="d-flex flex-row w-100">
-                                                <div className="my-2">
-                                                    {comentario.user_data.profile_photo_url == undefined ?
-                                                        <img src="https://picsum.photos/50/50" className="profile-photo" />
-                                                        :
-                                                        <img src={comentario.user_data.profile_photo_url} className="profile-photo " />
-                                                    }
-                                                </div>
-                                                <div className="d-flex align-items-center fw-bold">
-                                                    {comentario.user_data.username}
-                                                </div>
-                                                <div className="w-100 d-flex justify-content-end align-items-center">
-                                                    {commentStars.map((element, index) => {
-                                                        return (<i key={index} className={`${element} fa-star`}></i>)
-                                                    })}
+                                                    </button>
+                                                )
+                                            })}
+                                        </div>
+                                    </div>
+
+                                </div>
+                                {/* Published Comments */}
+                                <div className="container-fluid row div-service-comment">
+                                    {/* Header */}
+                                    <div className="border-bottom items border-0">
+                                        {store.comments.length <= 0
+                                            ? "No hay comentarios en esta publicación"
+                                            : `${store.comments.length} Comentarios`}
+                                    </div>
+                                    {/* Comment Generator */}
+                                    {store.comments.map((comentario, index) => {
+                                        let commentStars = []
+                                        for (let index = 0; index < starsState.stars.length; index++) {
+                                            if (index < comentario.rating) {
+                                                const element = starVariant
+                                                commentStars.push(element)
+                                            } else {
+                                                commentStars.push("fa-regular")
+                                            }
+                                        }
+                                        return (
+                                            <div key={index} className="border-bottom d-flex align-item-center py-2 col-lg-6 col-sm-12">
+                                                <div className="w-100 ">
+                                                    <div className="d-flex flex-row w-100">
+                                                        <div className="my-2">
+                                                            {comentario.user_data.profile_photo_url == undefined ?
+                                                                <img src="https://picsum.photos/50/50" className="profile-photo" />
+                                                                :
+                                                                <img src={comentario.user_data.profile_photo_url} className="profile-photo " />
+                                                            }
+                                                        </div>
+                                                        <div className="d-flex align-items-center fw-bold">
+                                                            {comentario.user_data.username}
+                                                        </div>
+                                                        <div className="w-100 d-flex justify-content-end align-items-center">
+                                                            {commentStars.map((element, index) => {
+                                                                return (<i key={index} className={`${element} fa-star`}></i>)
+                                                            })}
+                                                        </div>
+                                                    </div>
+                                                    <p>{comentario.observation}</p>
                                                 </div>
                                             </div>
-                                            <p>{comentario.observation}</p>
-                                        </div>
-
-                                );
-                            })}
+                                        );
+                                    })}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-        </>);
+        </>
+    );
 };
